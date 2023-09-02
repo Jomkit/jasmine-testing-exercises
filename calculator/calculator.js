0ac349efab4538e67f3a8fc9ab5ec920d1eba9b1
Jomkit <j.jujaroen96@gmail.com>
2023-09-03 00:05:41 +0900

    added code to calculator.js, but need to fix the equation for monthly payments under function calculateMonthlyPayments(values)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,6 +21,10 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const UIValues = getCurrentUIValues();
+  console.log(UIValues);
+
+  return calculateMonthlyPayment(UIValues);
 }
 
 // Get the current values from the UI
@@ -32,6 +36,14 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const p = values.amount;
+  const i = values.rate/12;
+  const n = values.years*12;
+  console.log(`principle amount: ${p}, monthly rate: ${i}, total # of payments: ${n}`);
+
+  const monthlyPayment = (p*i)/(1-((1+i)^n));
+  console.log("monthly payments are:", monthlyPayment);
+  return "monthly payments are", monthlyPayment;
 }
 
 // Given a string representing the monthly payment value,
