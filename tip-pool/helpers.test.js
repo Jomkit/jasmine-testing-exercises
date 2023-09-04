59398c626a09336a123125fd1546519818bfc484
Jomkit <j.jujaroen96@gmail.com>
2023-09-04 16:23:38 +0900

    added tests for helper and payments function in tip-pool

---
+++ tip-pool/helpers.test.js
@@ -0,0 +1,55 @@
+describe('helpers.js tests', () => {
+    beforeEach(function(){
+        billAmtInput.value = 100;
+        tipAmtInput.value = 10;
+        submitPaymentInfo();
+    })
+    
+    it('should sum the bill amount of all payments and return the total on sumPaymentTotal()', () => {
+        expect(sumPaymentTotal('billAmt')).toEqual(100);
+
+        billAmtInput.value = 300;
+        tipAmtInput.value = 40;
+
+        submitPaymentInfo();
+        
+        expect(sumPaymentTotal('billAmt')).toEqual(400);
+    })
+
+    it('should sum the tip amount of all payments and return the total on sumPaymentTotal()', () => {
+        expect(sumPaymentTotal('tipAmt')).toEqual(10);
+
+        billAmtInput.value = 200;
+        tipAmtInput.value = 10;
+
+        submitPaymentInfo();
+
+        expect(sumPaymentTotal('tipAmt')).toEqual(20);
+    })
+
+    it('should calculate tip percent given bill amt and tip amt on calculateTipPercent()', () => {
+        expect(calculateTipPercent(400, 25)).toEqual(6);
+        expect(calculateTipPercent(30, 3)).toEqual(10);
+    })
+
+    it('should add new cells to a table row on appendTd()', () => {
+        let testTr = document.createElement('tr');
+        
+        appendTd(testTr, 'test');
+        expect(testTr.children.length).toEqual(1);
+        expect(testTr.firstChild.innerText).toEqual('test');
+    })
+    
+    afterEach(() => {
+        billAmtInput.value = '';
+        tipAmtInput.value = '';
+        summaryTds[0].innerHTML = '';
+        summaryTds[1].innerHTML = '';
+        summaryTds[2].innerHTML = '';
+        serverTbody.innerHTML = '';
+        paymentTbody.innerHTML = '';
+        serverId = 0;
+        allPayments = {};
+        paymentId = 0;
+    })
+})
--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -10,6 +10,37 @@ describe('Payments.js tests', () => {
         expect(Object.keys(allPayments).length).toEqual(1);
         expect(allPayments['payment1'].billAmt).toEqual('5');
     })
+
+    it('should create a curPayment object with bill amount, tip amount, and tip percent on createCurPayment()', () => {
+        let curPayment = createCurPayment();
+        let billAmt = curPayment.billAmt;
+        let tipAmt = curPayment.tipAmt;
+        let tipPercent = curPayment.tipPercent;
+
+        expect(Object.keys(curPayment).length).toEqual(3);
+        expect(billAmt).toEqual('5');
+        expect(tipAmt).toEqual('1');
+        expect(tipPercent).toEqual(20);
+    })
+
+    it('should append an entry to the payment table on appendPaymentTable()', () => {
+        appendPaymentTable(createCurPayment());
+        
+        let curPaymentList = document.querySelectorAll('#paymentTable tbody tr td');
+
+        expect(curPaymentList.length).toEqual(3);
+        expect(curPaymentList[0].innerText).toEqual('$5');
+        expect(curPaymentList[1].innerText).toEqual('$1');
+        expect(curPaymentList[2].innerText).toEqual('20%');
+    })
+
+    it('should add new entry under summary on updateSummary()', () => {
+        submitPaymentInfo();
+        updateSummary();
+        expect(summaryTds[0].innerText).toEqual('$5');
+        expect(summaryTds[1].innerText).toEqual('$1');
+        expect(summaryTds[2].innerText).toEqual('20%');
+    })
     
     afterEach(() => {
         billAmtInput.value = '';
